28d1ffb4f458fbac3cba5fbf78be2ee230fb48ea
eduardomantz291 <eduardomantz2112@gmail.com>
2020-09-19 00:58:30 -0300

    primeira aplicação com electronjs

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const pass = document.querySelector('#pass');
+const newPassword = document.querySelector("#newPassword");
+
+function generatePass() {
+  newPassword.innerHTML = 'Senha: '
+
+  if (pass.value === '') {
+    alert("Digite o tamanho da sua senha!")
+    return;
+  }
+
+  let charset = 'abcdefghijkmnopqrstuvxwyzABCDEFGHHIJKLMNOPQRSTUVWXYZ0123456789';
+  let password = "";
+
+  for (var i = 0, n = charset.length; i < pass.value; ++i){
+    password += charset.charAt( Math.floor( Math.random() * n ));
+  }
+
+  let result = document.createTextNode(password);
+  return newPassword.appendChild(result);
+}
